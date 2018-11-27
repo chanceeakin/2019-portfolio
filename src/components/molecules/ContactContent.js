d06b7e7a97db3de39fa6c3cda69c73ed00954434
Chance Eakin <chance.eakin@gmail.com>
2018-11-26 18:48:29 -0600

    more updates

--- src/components/molecules/ContactContent.js
+++ src/components/molecules/ContactContent.js
@@ -29,15 +29,11 @@ const Contact = () => (
     <Inner>
       <Title>Get in touch</Title>
       <ContactText>
-        Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-        <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-        <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+        Say <a href="mailto:chance.eakin@gmail.com">Hi</a> or find me on{' '}
+        <a href="https://www.instagram.com/chanceeakin">Instagram</a>.
       </ContactText>
     </Inner>
-    <Footer>
-      &copy; 2018 by Gatsby Starter Portfolio Cara.{' '}
-      <a href="https://github.com/LeKoArts/gatsby-starter-portfolio-cara">Github Repository</a>.
-    </Footer>
+    <Footer>&copy; 2018 by Chance Eakin.</Footer>
   </Content>
 );
 
